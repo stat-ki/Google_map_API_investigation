ac7e6180993abaf0ac0347701213cd6023cd8398
stat-ki <per_ik@outlook.com>
2020-05-19 04:25:04 +0000

    Finish

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,87 +1,79 @@
-// let map;
+$(function () {
+	var map;
+	var service;
+	var infowindow;
+	var pyrmont = new google.maps.LatLng(35.67832667, 139.77044378);
+	createMap(pyrmont)
+	document.getElementById('getcurrentlocation').onclick = function() {
+		geoLocationInit();
+	}
 
-// function initMap(){
-//   // geocoder = new google.maps.Geocoder()
+	function geoLocationInit() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, fail);
+		} else {
+			createMap(pyrmont);
+		}
+	}
 
-//   // map = new google.maps.Map(document.getElementById('map'), {
-//   //   center: {lat: 40.7828, lng:-73.9653},
-//   //   zoom: 12,
-//   // });
+	function success(position) {
+		var currentLat = position.coords.latitude;
+		var currentLng = position.coords.longitude;
+		var pyrmont = new google.maps.LatLng(currentLat,currentLng);
+		createMap(pyrmont)
+		CurrentPositionMarker(pyrmont);
+	}
 
-//   // marker = new google.maps.Marker({
-//   //   position:  {lat: 40.7828, lng:-73.9653},
-//   //   map: map
-//   // });
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(
-//       function(position) {
-//         var mapLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-//         var mapOptions = {
-//           zoom : 15,
-//           center : mapLatLng
-//         };
-//         var map = new google.maps.Map(
-//           document.getElementById("map"),
-//           mapOptions
-//         );
-//         var marker = new google.maps.Marker({
-//           map : map,
-//           position : mapLatLng
-//         });
-//       },
-//       function(error) {
-//         switch(error.code) {
-//           case 1:
-//             alert("Permission denied");
-//             break;
-//           case 2:
-//             alert("Location not identified");
-//             break;
-//           case 3:
-//             alert("Time out");
-//             break;
-//           default:
-//             alert("Unexpected Error");
-//             break;
-//         }
-//       }
-//     );
-//   } else {
-//     alert("Devise error");
-// 	}
-// }
+	function fail(pyrmont) {
+		createMap(pyrmont);
+	}
 
-var map;
-var service;
-var infowindow;
+	function createMap(pyrmont) {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: pyrmont,
+			zoom: 15
+		});
+		nearbysearch(pyrmont)
+	}
 
-function initMap() {
-	var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-	navigator.geolocation.getCurrentPosition(geoSuccess);
+	function createMarker(latlng, id, place) {
+		var marker = new google.maps.Marker({
+			position: latlng,
+			map: map
+		});
+		var placename = place.name;
+		var infoWindow = new google.maps.InfoWindow({
+			content: id
+		});
+		marker.addListener('click', function() {
+			infoWindow.open(map, marker);
+		});
+	}
 
-  infowindow = new google.maps.InfoWindow();
+	function CurrentPositionMarker(pyrmont) {
+		var marker = new google.maps.Marker({
+			position: pyrmont,
+			map: map
+		});
+		marker.setMap(map);
+	}
 
-  map = new google.maps.Map(
-      document.getElementById('map'), {center: current_position, zoom: 15});
-
-  var request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
-  };
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
+	function nearbysearch(pyrmont) {
+		var request = {
+			location: pyrmont,
+			radius: '1000'
+		};
+		service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, callback);
+		function callback(results, status) {
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				for (var i = 0; i < results.length; i++) {
+					var place = results[i];
+					var latlng = place.geometry.location;
+					var id = place.id;
+					createMarker(latlng, id, place);
+				}
+			}
+		}
+	}
+});
